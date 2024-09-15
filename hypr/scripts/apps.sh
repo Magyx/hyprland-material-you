96dfdfee53aef00771ba68cb73c4861fca86969b
magyx <w.magyx@gmail.com>
2024-09-15 18:06:35 +0200

    Add option support

--- hypr/scripts/apps.sh
+++ hypr/scripts/apps.sh
@@ -6,13 +6,16 @@ JSON_FILE="$HOME/dotfiles/.settings/apps.json"
 # Read the application type from the argument
 APP_TYPE=$1
 
+# Remove the first argument, keeping the rest as options for the app
+shift
+
 # Extract the corresponding application value from the JSON file using jq
 APP=$(jq -r ".$APP_TYPE" "$JSON_FILE")
 
 # Check if the application is defined in the JSON file
 if [ "$APP" != "null" ]; then
     # Open the specified application
-    $APP
+    $APP "$@"
 else
     echo "No application defined for $APP_TYPE in $JSON_FILE"
 fi
