493709d4e2176b4e7075abc98cdbc7f196be584a
magyx <w.magyx@gmail.com>
2024-09-29 13:51:17 +0200

    Install customized hyprland config

---
+++ hypr/scripts/scratch/term-top.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Define a function to run the filtered watch command
+run_hyprctl_watch() {
+    watch -twn 1.0 'hyprctl clients | grep -E "(Window|workspace:|class:|pid:|xwayland:|fullscreen:)" | awk '\''BEGIN {block=""; skip=0;} /^Window/ {if (block != "" && skip == 0) {print block} block=$0; skip=0; next} {block = block "\n" $0; if ($0 ~ /scratch/) {skip=1}} END {if (block != "" && skip == 0) {print block}}'\'''
+}
+
+# Export the function to make it available in subshells
+export -f run_hyprctl_watch
+
+# Launch tmux windows
+tmux new-session -d -s monitor -n 'btop' "bash -c 'btop'"
+tmux new-window -t monitor:1 -n 'nvidia_smi' "bash -c 'watch -twn 1.0 nvidia-smi'"
+tmux new-window -t monitor:2 -n 'hyprctl' bash -c run_hyprctl_watch
+tmux new-window -t monitor:3 -n 'custom'
+
+# Attach to the session
+tmux attach-session -t monitor
